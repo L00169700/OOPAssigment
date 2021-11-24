97cd40c2b633dfaae6d5ae5202d15c88280776e6
L00169700 <91153952+L00169700@users.noreply.github.com>
2021-11-24 13:21:16 +0000

    Update L00169700_Q4_File_2.py
    added top comments

--- L00169700_Q4_File_2.py
+++ L00169700_Q4_File_2.py
@@ -1,3 +1,15 @@
+"""
+# 
+# File : L00169700_Q4_File_2.py
+# Created ：24.11.21
+# Author ：R.Lima
+# Version ：v1.0.0
+# Licencing : (C) 2021 R.Lima, LYIT
+# Available under GNU Public License (GPL)
+# Description ：Determine which ports are open and display the 
+# information in a tidy format.
+"""
+
 import paramiko
 import time
 import re
